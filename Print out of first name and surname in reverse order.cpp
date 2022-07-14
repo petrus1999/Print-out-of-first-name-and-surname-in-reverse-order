f3749648550dd962a79db5726dfb8518c34d31b4
petrus <pshai671@gmail.com>
2022-07-14 08:18:27 +0200

    Add project files.

---
+++ Print out of first name and surname in reverse order.cpp
@@ -0,0 +1,31 @@
+// Print out of first name and surname in reverse order.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    char name[30], surname[30];
+
+    cout << "Enter your name " << endl;
+    cin >> name;
+    cout << "Enter your surname " << endl;
+    cin >> surname;
+
+    cout << "Your full names in reverse is:" << surname << " " << name;
+
+    return 0;
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
